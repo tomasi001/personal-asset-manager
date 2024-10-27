2934d902a014960647042637bf6d5c9b5572184a
tomasi001 <tom.shields001@gmail.com>
2024-10-27 17:41:34 +0200

    create initial asset and user tests

---
+++ src/asset/asset.service.spec.ts
@@ -0,0 +1,19 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AssetService } from './asset.service';
+import { DatabaseService } from '../database/database.service';
+
+describe('AssetService', () => {
+  let service: AssetService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [AssetService, DatabaseService],
+    }).compile();
+
+    service = module.get<AssetService>(AssetService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
---
+++ src/user/user.service.spec.ts
@@ -0,0 +1,19 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserService } from './user.service';
+import { DatabaseService } from '../database/database.service';
+
+describe('UserService', () => {
+  let service: UserService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [UserService, DatabaseService],
+    }).compile();
+
+    service = module.get<UserService>(UserService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
