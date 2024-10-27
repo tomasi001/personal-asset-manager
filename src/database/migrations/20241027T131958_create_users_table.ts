17d728d66073af1fdb89521bcda90e3f3df07266
tomasi001 <tom.shields001@gmail.com>
2024-10-27 13:40:28 +0200

    create users table migration

---
+++ src/database/migrations/20241027T131958_create_users_table.ts
@@ -0,0 +1,18 @@
+import { Kysely, sql } from 'kysely';
+
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createTable('users')
+    .addColumn('id', 'uuid', (col) =>
+      col.primaryKey().defaultTo(sql`gen_random_uuid()`),
+    )
+    .addColumn('email', 'varchar(255)', (col) => col.notNull().unique())
+    .addColumn('privy_id', 'varchar(255)', (col) => col.notNull().unique())
+    .addColumn('created_at', 'timestamptz', (col) => col.defaultTo(sql`now()`))
+    .addColumn('updated_at', 'timestamptz', (col) => col.defaultTo(sql`now()`))
+    .execute();
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropTable('users').execute();
+}
