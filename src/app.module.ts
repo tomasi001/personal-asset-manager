6d270d05db07fe9e7c3fb3685063674b8dbf38ed
tomasi001 <tom.shields001@gmail.com>
2024-10-27 17:16:02 +0200

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,27 @@
 // src/app.module.ts
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { AssetModule } from './asset/asset.module';
+import { UserModule } from './user/user.module';
+import { UserIdMiddleware } from './auth/user_id.middleware';
 
 @Module({
   imports: [
     AuthModule,
     ConfigModule.forRoot({ isGlobal: true }),
-    DatabaseModule, // Load .env variables globally
+    DatabaseModule,
+    AssetModule,
+    UserModule, // Load .env variables globally
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(UserIdMiddleware).forRoutes('*');
+  }
+}
