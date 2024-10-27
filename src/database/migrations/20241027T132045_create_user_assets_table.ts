6a0eec6080381f32df4bb211586e6ba88770dea4
tomasi001 <tom.shields001@gmail.com>
2024-10-27 13:40:51 +0200

    create user assets table migration

---
+++ src/database/migrations/20241027T132045_create_user_assets_table.ts
@@ -0,0 +1,22 @@
+import { Kysely, sql } from 'kysely';
+
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createTable('user_assets')
+    .addColumn('id', 'uuid', (col) =>
+      col.primaryKey().defaultTo(sql`gen_random_uuid()`),
+    )
+    .addColumn('user_id', 'uuid', (col) =>
+      col.references('users.id').onDelete('cascade'),
+    )
+    .addColumn('asset_id', 'uuid', (col) =>
+      col.references('assets.id').onDelete('cascade'),
+    )
+    .addColumn('quantity', 'numeric(20, 6)', (col) => col.defaultTo(0))
+    .addColumn('created_at', 'timestamptz', (col) => col.defaultTo(sql`now()`))
+    .execute();
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropTable('user_assets').execute();
+}
